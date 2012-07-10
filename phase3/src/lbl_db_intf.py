97df3908bba6b35712c783790dc9d2ddaf0d5381
Anders Spælling <spaelling@gmail.com>
2012-07-10 15:48:38 +0200

    rc

--- phase3/src/lbl_db_intf.py
+++ phase3/src/lbl_db_intf.py
@@ -15,13 +15,14 @@ LABELS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
 def is_overlapping(a,b):
 
-	return a.get('ytid') == b.get('ytid') and a.get('l') != b.get('l') and a != b and max(a.get('s')[0],b.get('s')[0]) < min(a.get('s')[1],b.get('s')[1])
+	# return a.get('ytid') == b.get('ytid') and a.get('l') != b.get('l') and a != b and max(a.get('s')[0],b.get('s')[0]) < min(a.get('s')[1],b.get('s')[1])
+	return max(a.get('s')[0],b.get('s')[0]) < min(a.get('s')[1],b.get('s')[1])
 
 def validate_dummy_segment(ds, dummies):
 
 	for dummy in dummies:
-		if not is_overlapping(dummy, ds):
-			return True
+		if ds.get('ytid') == dummy.get('ytid') and ds.get('l') == dummy.get('l') and is_overlapping(dummy, ds):
+			return False
 	return True
 
 def gen_dummy_segment(labels, ytids, dummies, min_segment_length=36):
@@ -29,7 +30,7 @@ def gen_dummy_segment(labels, ytids, dummies, min_segment_length=36):
 	for i in range(5):
 		a = random.randint(0,2159-min_segment_length)
 		b = random.randint(a+1,2160)
-		l = random.choice(labels)
+		l = [random.choice(labels)]
 		ytid = random.choice(ytids)
 		ds = dict(s=(a,b), l=l, ytid=ytid)
 		# need to check that the new dummy segment does not overlap an existing segment
@@ -66,43 +67,58 @@ def get_overlap(a,b):
 	segment = (max(a.get('s')[0],b.get('s')[0]), min(a.get('s')[1],b.get('s')[1]))
 	labels = sorted(a.get('l') + b.get('l'))
 	ytid = a.get('ytid')
+	# return dict(s=segment, l=labels, ytid=ytid, org=(a,b))
 	return dict(s=segment, l=labels, ytid=ytid)
 
 def get_overlapping_segments(labels, candidates, min_segment_length=36):
 
-	_candidates = []
-	for a in candidates:
-		for b in candidates:
-			if is_overlapping(a,b):
-				c = get_overlap(a,b)
-				# avoid duplicates and too short segments
-				if get_segment_len(c) > min_segment_length and c not in _candidates:
-					_candidates.append(c)
+	# check if labels from a is present in labels from b
+	def f(a,b):
+		for l in a.get('l'):
+			if l in b.get('l'):
+				return False
+		return True
 
-	return _candidates
+	# compute candidates based on input candidates. then reiterate new candidates to produce even more
+	for x in range(len(labels)-1):
+		# copy the current candidates
+		_candidates = list(candidates)
+		# optimized loops
+		for i in range(len(_candidates)):
+			a = _candidates[i]
+			for j in range(i+1, len(_candidates)):
+				b = _candidates[j]
+				# assert(a!=b)
+				if a.get('ytid') == b.get('ytid') and is_overlapping(a,b) and f(a,b):
+					c = get_overlap(a,b)
+					# avoid duplicates and too short segments - the larger "candidates", the longer it takes to validate
+					# duplicates will occur if we produce candidates with more than 2 labels
+					if get_segment_len(c) > min_segment_length and c not in candidates:
+						candidates.append(c)
+		# possible optimization is to filter out candidates with only 1 label (need to do this only once)
+	return candidates
 
 def sort(candidates):
 
-	return sorted(candidates, key=lambda x: len(x['l']), reverse=True)
+	# Starting with Python 2.2, sorts are guaranteed to be stable. That means that when multiple records have the same key, their original order is preserved.
+	candidates = sorted(candidates, key=lambda x: x.get('ytid'), reverse=True)
+	candidates = sorted(candidates, key=lambda x: get_segment_len(x), reverse=True)
+	candidates = sorted(candidates, key=lambda x: len(x['l']), reverse=True)
+	return candidates
 
 def get_segments(labels):
 
 	candidates = []
 	for label in labels:
 		candidates += _get_with_label(label)
-	
-	candidates += get_overlapping_segments(labels, candidates)
 
+	candidates = get_overlapping_segments(labels, candidates)
 	candidates = sort(candidates)
 	return candidates
 
 def main():
 
-	global db
-	# #segments, #labels, #videos
-	db, dummy_labels = create_dummy_date(2500, 7, 75)
-
-	labels = sorted(random.sample(dummy_labels, 3))
+	labels = sorted(random.sample(dummy_labels, 4))
 	print 'test labels: ', labels
 	print 'DB size: %d, %dkb' % (len(db), len(json.dumps(db))/1024)
 
@@ -111,8 +127,14 @@ def main():
 
 	# candidates = get_segments(labels)
 	print '#candidates: %d' % len(candidates)
-	# print 'candidates: ', json.dumps(candidates[:5], sort_keys=True, indent=4)
+	# print 'candidates: ', json.dumps(candidates[:4], sort_keys=True, indent=4)
 
 if __name__ == '__main__':
+	global db
+	# #segments, #labels, #videos
+	print 'generating dummy data...'
+	db, dummy_labels = create_dummy_date(2500, 7, 75)
+	print 'done!'
+
 	# cProfile.run('main()')
 	main()
