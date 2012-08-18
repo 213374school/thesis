f1a6140ece687f611214a439a01281d86e3af72a
Anders Spælling <spaelling@gmail.com>
2012-08-18 21:20:39 +0200

    dsada

--- phase4/st_test.py
+++ phase4/st_test.py
@@ -6,7 +6,9 @@ import pylab
 import random
 import json
 import matplotlib
+import math
 
+exp = np.exp
 plt = matplotlib.pyplot
 
 def smoothTriangle(lst=[], degree=1):
@@ -47,56 +49,118 @@ def smoothTriangle(lst=[], degree=1):
 
 def main():
 
-	xs = [0, 0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.5, 0.6, 0.7, 0.2, 0.2, 0.25, 0.3, 0.35, 0.40, 0.3, 0.2]
-	def inc_resolution(xs, factor=10):
+	# xs = [0, 0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.5, 0.6, 0.7, 0.2, 0.2, 0.25, 0.3, 0.35, 0.40, 0.3, 0.2]
+	# def inc_resolution(xs, factor=10):
 
-		zs = []
-		for i in range(len(xs)-1):
-			x = xs[i]
-			y = xs[i+1]
-			z = np.linspace(x,y,factor)
-			zs += list(z)
-		return zs
-	xs = inc_resolution(xs, 12)
+	# 	zs = []
+	# 	for i in range(len(xs)-1):
+	# 		x = xs[i]
+	# 		y = xs[i+1]
+	# 		z = np.linspace(x,y,factor)
+	# 		zs += list(z)
+	# 	return zs
+	# xs = inc_resolution(xs, 12)
+
+	# pylab.figure()		
+	# pylab.suptitle('Triangle smoothing', fontsize=16)
+
+	# pylab.subplot(2,2,1, title='Original data')  
+	# pylab.plot(xs,".k")  
+	# pylab.plot(xs,"-k")
+	# pylab.axis()  
+	# pylab.grid(True)
+
+	# d = 6
+	# xssmooth = smoothTriangle(xs, d)
+	# title = 'Smoothed with w = %d' % d
+	# pylab.subplot(2,2,2, title=title)  
+	# pylab.plot(xssmooth,".k")  
+	# pylab.plot(xssmooth,"-k")
+	# pylab.axis()  
+	# pylab.grid(True)
+
+	# d = 12
+	# xssmooth = smoothTriangle(xs, d)
+	# title = 'Smoothed with w = %d' % d
+	# pylab.subplot(2,2,3, title=title)  
+	# pylab.plot(xssmooth,".k")  
+	# pylab.plot(xssmooth,"-k")
+	# pylab.axis()  
+	# pylab.grid(True)
+
+	# d = 24
+	# xssmooth = smoothTriangle(xs, d)
+	# title = 'Smoothed with w = %d' % d
+	# pylab.subplot(2,2,4, title=title)  
+	# pylab.plot(xssmooth,".k")  
+	# pylab.plot(xssmooth,"-k")
+	# pylab.axis()  
+	# pylab.grid(True)
+
+	# format = 'png'
+	# plt.savefig('triangle_smooth.png', format=format)
+
+	# pylab.show()
+
+	xs = np.linspace(0,1,250)
+	ys = np.log(xs) + 10
+
+	def normalize(a, factor=255.0):
+
+		# convert to floating point	
+		if not type(a).__name__ == 'numpy.ndarray':
+			# a is not a numpy array
+			b = np.float64(np.array(a)).copy()
+		else:
+			b = np.float64(a.copy())
+		
+		bmin = np.min(b)
+		if bmin > 0.0:
+			# no point in substracting 0
+			b -= np.min(b)
+
+		bmax = np.max(b)
+		if bmax > 0.0:
+			# to avoid dividing by zero
+			b *= factor / bmax
+		else:
+			return a.copy()
+
+		# convert back to integer
+		return b # np.int64(b)	
+
+	ys = normalize(ys, 1.0)
 
 	pylab.figure()		
-	pylab.suptitle('Triangle smoothing', fontsize=16)
+	pylab.suptitle('TITEL', fontsize=16)
 
-	pylab.subplot(2,2,1, title='Original data')  
-	pylab.plot(xs,".k")  
-	pylab.plot(xs,"-k")
+	pylab.subplot(2,2,1, title='ln(x)')  
+	pylab.plot(xs,ys,".k")  
+	pylab.plot(xs,ys,"-k")
 	pylab.axis()  
 	pylab.grid(True)
 
-	d = 6
-	xssmooth = smoothTriangle(xs, d)
-	title = 'Smoothness degree = %d' % d
-	pylab.subplot(2,2,2, title=title)  
-	pylab.plot(xssmooth,".k")  
-	pylab.plot(xssmooth,"-k")
+	ys = ys - xs
+	pylab.subplot(2,2,2, title='ln(x)-x')  
+	pylab.plot(xs,ys,".k")  
+	pylab.plot(xs,ys,"-k")
+	pylab.axis()  
+	pylab.grid(True)	
+
+	ys = np.exp(xs)
+	ys = normalize(ys, 1.0)
+	pylab.subplot(2,2,3, title='exp(x)')  
+	pylab.plot(xs,ys,".k")  
+	pylab.plot(xs,ys,"-k")
 	pylab.axis()  
 	pylab.grid(True)
 
-	d = 12
-	xssmooth = smoothTriangle(xs, d)
-	title = 'Smoothness degree = %d' % d
-	pylab.subplot(2,2,3, title=title)  
-	pylab.plot(xssmooth,".k")  
-	pylab.plot(xssmooth,"-k")
+	ys = ys - xs
+	pylab.subplot(2,2,4, title='exp(x)-x')  
+	pylab.plot(xs,ys,".k")  
+	pylab.plot(xs,ys,"-k")
 	pylab.axis()  
-	pylab.grid(True)
-
-	d = 24
-	xssmooth = smoothTriangle(xs, d)
-	title = 'Smoothness degree = %d' % d
-	pylab.subplot(2,2,4, title=title)  
-	pylab.plot(xssmooth,".k")  
-	pylab.plot(xssmooth,"-k")
-	pylab.axis()  
-	pylab.grid(True)
-
-	format = 'png'
-	plt.savefig('triangle_smooth.png', format=format)
+	pylab.grid(True)	
 
 	pylab.show()
 
