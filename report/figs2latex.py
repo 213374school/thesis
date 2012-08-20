fa9183408b956ec50f3b4dbf3f28f4620e2049e6
Anders Spælling <spaelling@gmail.com>
2012-08-20 17:52:35 +0200

    dsadsa

--- report/figs2latex.py
+++ report/figs2latex.py
@@ -37,22 +37,78 @@ from matrix2latex_lib import matrix2latex
 def main():
 
 	path = './img/p1figs'
-	filenames = [filename for filename in os.listdir(path) if filename.split('.')[-1] in ['png'] and '#' not in filename]	
+	filenames = [filename for filename in os.listdir(path) if filename.split('.')[-1] in ['png'] and '#' not in filename and 'computeFrameStateAnders' not in filename and 'computeFrameStateCubic' not in filename]	
+
+	def get_caption(filename):
+
+		if 'computeFrameStateContrastOnly' in filename:
+			caption = 'CO'
+		elif 'computeFrameStateCummulative' in filename:
+			caption = 'CCSM'
+		elif 'computeFrameStateLauge' in filename:
+			caption = 'ICSM '
+		elif 'computeFrameStateMagnitudeOnly' in filename:
+			caption = 'MO'
+
+		if 'sectionCompareBetter' in filename:
+			if 'smooth0' in filename:
+				if 'tol12' in filename:
+					caption += '7'
+				if 'tol24' in filename:
+					caption += '3'
+				if 'tol48' in filename:
+					caption += '4'					
+			elif 'smooth12' in filename:
+				if 'tol24' in filename:
+					caption += '5'
+				if 'tol48' in filename:
+					caption += '6'	
+
+		elif 'simpleCompare' in filename:
+			if 'smooth0' in filename:
+				caption += '1'
+			elif 'smooth12' in filename:
+				caption += '2'
+		if 'AUC' in filename:
+			caption += ' (AUC)'
+		return caption
+
+	sortme = [(get_caption(f),f) for f in filenames]
+	sortme = sorted(sortme)
+	filenames = [f for c,f in sortme]
+
+	subfigs_per_fig = 4
+	subfigs_per_row = 2
 
 	out = '%\n'
-	for filename in filenames:
+	for i, filename in enumerate(filenames):
+		# if i > 0 and i % 6 == 0:
+		# 	out += '\FloatBarrier\n'
+
 		print filename
-		# filename = filename.replace('#','\#')
-		caption = 'caption'
-		# out += '%\n'
-		out += '\\begin{figure}[!ht]\n'
-		out += '\t\\centering\n'
-		out += '\t\\includegraphics[width=0.5\\textwidth]{img/p1figs/%s}\n' % filename
-		out += '\t\\caption{%s}\n' % caption
-		# out += '\t\\label{fig:%s}\n' % filename
-		out += '\\end{figure}\n'
-		out += '%\n'
-	print out
+		caption = get_caption(filename)
+		print caption
+
+		if i > 0 and i % subfigs_per_fig == 0:
+			out += '\\end{figure}\n'
+			out += '%\n'		
+		if i % subfigs_per_fig == 0:
+			out += '\\begin{figure}[!ht]\n'
+			out += '\t\\centering\n'
+		
+		out += '\t\\subfigure[%s]{\n' % caption
+		# out += '\t\t\\label{fig:%s}\n' % filename
+		out += '\t\t\\includegraphics[width=0.45\\textwidth]{img/p1figs/%s}\n' % filename
+		# out += '\t\t\\caption{%s}\n' % caption
+		out += '\t}'
+		if i > 0 and (i+1) % subfigs_per_row == 0:
+			out += '\\\\'
+		out += '\n'
+
+	out += '\\end{figure}\n'
+	out += '%\n'		
+
+	# print out
 
 	f = open('p1figs.tex','w')	
 	f.write(out)
